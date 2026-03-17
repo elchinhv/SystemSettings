87c716d8b551d39068766661d13763da088a5221
Elchin Heybatov <elchinh@gmail.com>
2026-03-17 12:07:56 +0400

    set signingConfig for release in build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,6 +40,7 @@ android {
 
     buildTypes {
         release {
+            signingConfig = signingConfigs.getByName("release")
             isMinifyEnabled = true
             isShrinkResources = true
             proguardFiles(
