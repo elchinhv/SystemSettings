10d0bb898124854fcd74f522586597058bd1456a
Elchin Heybatov <elchinh@gmail.com>
2026-03-17 11:32:37 +0400

    enable edge to edge in  MainActivity

--- app/src/main/java/com/olcane/systemsettings/MainActivity.kt
+++ app/src/main/java/com/olcane/systemsettings/MainActivity.kt
@@ -6,10 +6,12 @@ import android.content.Intent
 import android.os.Bundle
 import android.provider.Settings
 import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        enableEdgeToEdge()
         super.onCreate(savedInstanceState)
         openSystemSettings()
     }
